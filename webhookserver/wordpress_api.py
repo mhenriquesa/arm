195802de51f2b768fad4709b424182bd19b027c5
M. Henrique S.A <henriqueator@gmail.com>
2022-01-03 00:37:41 -0300

    upload images using wp api and retrive the url

--- webhookserver/wordpress_api.py
+++ webhookserver/wordpress_api.py
@@ -1,12 +1,15 @@
 import requests
 from os import getenv
+import json
 
 token = getenv('ARM_WP_API')
-url = "https://anaramosmoda.com.br/wp-json/wp/v2/posts"
+url = "https://anaramosmoda.com.br/wp-json/wp/v2/media"
 headers = {'Authorization': token}
 data = {
-    "title": "post teste"
+    "file": open('d:\Programming\Projetos\\armbot\webhookserver\image.jpg', 'rb')
 }
 
-response = requests.post(url, json=data, headers=headers)
-print(response)
+response = requests.post(url, files=data, headers=headers)
+# response = requests.post(url, json=data, headers=headers)
+imgurl = str(json.loads(response.content)['source_url'])
+print(imgurl)
